f49eb8218cdaa3d7e55465995fd1c1fcd5ebd213
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2019-03-13 17:01:56 +0300

    Сheck

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
-main file
+#include "Header.h"
+int main()
+{
+	DQ Temp(10);
+	int k = 0;
+	Temp.insert(3);
+	Temp.insert(2);
+	Temp.insert(8);
+	Temp.insert(9);
+	Temp.insert(1);
+	Temp.insert(5);
+	Temp.insert(4);
+	Temp.insert(9);
+	Temp.insert(5);
+	Temp.insert(7);
+	Temp.show();
+	Temp.deleteRoot();
+	Temp.deleteCur(3);
+	Temp.show();
+	return 0;
+}
